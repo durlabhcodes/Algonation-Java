24ab18226944170e3c6da50ff345fe3167aceade
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2017-09-07 14:40:27 +0530

    Added QuickSort.

---
+++ algonation/src/main/java/com/algonation/sorting/sorters/QuickSort.java
@@ -0,0 +1,49 @@
+package com.algonation.sorting.sorters;
+
+import com.algonation.sorting.util.SortUtil;
+
+public class QuickSort implements ISorter {
+
+	@Override
+	public Double[] sort(Double[] array) {
+		
+		int len = array.length;
+		int low = 0;
+		int high = len-1;
+		quickSort(array, low, high);
+		
+		return array;
+	}
+
+	private void quickSort(Double[] array, int low, int high) {
+		
+		if(low<high) {
+			int partition = partition(array, low, high);
+			quickSort(array, low, partition-1);
+			quickSort(array, partition+1, high);
+		}
+	}
+	private int partition(Double[] array, int low, int right) {
+		Double pivot = array[right];
+		int high=right-1;
+		while(true) {
+			while(array[low] < pivot && low<=high) {
+				low++;
+			}
+			while(array[high] > pivot && low<=high) {
+				high--;
+			}
+			
+			if(low>high) {
+				break;
+			} else {
+				SortUtil.swapByPosition(array, low, high);
+			}
+		}
+		SortUtil.swapByPosition(array, low, right);
+		return low;
+	}
+	
+	
+
+}
