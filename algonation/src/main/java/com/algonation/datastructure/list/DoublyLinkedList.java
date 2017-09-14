c2c0d4866ef62900d45a6071874ce206aa732021
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2017-09-14 18:35:31 +0530

    Implementing DoublyLinkedList

--- algonation/src/main/java/com/algonation/datastructure/list/DoublyLinkedList.java
+++ algonation/src/main/java/com/algonation/datastructure/list/DoublyLinkedList.java
@@ -1,21 +1,50 @@
 package com.algonation.datastructure.list;
 
-public class DoublyLinkedList<T> implements List<T> {
+public class DoublyLinkedList<K> implements List<K> {
+	
+	Node<K> head;
+	
+	public class Node<T> {
+		T element;
+		Node<T> next;
+		Node<T> previous;
+		
+		Node(T element) {
+			this.element = element;
+			this.next = null;
+			this.previous = null;
+		}
+		
+		@Override
+		public String toString() {
+			return this.element.toString();
+		}
+	}
 
 	@Override
-	public void add(T element) {
+	public void add(K element) {
+		if(head==null) {
+			head = new Node<K>(element);
+		} else {
+			Node<K> newNode = new Node<K>(element);
+			Node<K> node = head;
+			while(node.next != null) {
+				node = node.next;
+			}
+			node.next=newNode;
+			newNode.previous=node;
+		}
+		
+	}
+
+	@Override
+	public void addFirst(K element) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void addFirst(T element) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void add(int index, T element) {
+	public void add(int index, K element) {
 		// TODO Auto-generated method stub
 		
 	}
--- algonation/src/main/java/com/algonation/datastructure/list/ListMain.java
+++ algonation/src/main/java/com/algonation/datastructure/list/ListMain.java
@@ -3,8 +3,7 @@ package com.algonation.datastructure.list;
 public class ListMain {
 
 	public static void main(String[] args) {
-		LinkedList<Integer> l = new LinkedList<Integer>();
-		//java.util.LinkedList<Integer> x= new java.util.LinkedList<Integer>();
+		List<Integer> l = new LinkedList<Integer>();
 		
 		l.add(2);
 		l.add(3);
