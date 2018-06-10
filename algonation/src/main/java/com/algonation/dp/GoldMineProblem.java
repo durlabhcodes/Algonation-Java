9d0c8124d6dfc8e2fbf1b0728126b2112cb016b4
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2018-06-10 22:51:16 +0530

    Added path for gold mine problem

--- algonation/src/main/java/com/algonation/dp/GoldMineProblem.java
+++ algonation/src/main/java/com/algonation/dp/GoldMineProblem.java
@@ -1,8 +1,11 @@
 package com.algonation.dp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GoldMineProblem {
 
-	public static int maxProfit(int[][] arr) {
+	public int maxProfit(int[][] arr) {
 		int maxCol = 0;
 		for(int i=1; i<arr.length; i++) {
 			if(arr[i][0] > arr[maxCol][0]) {
@@ -12,7 +15,7 @@ public class GoldMineProblem {
 		return maxProfit(arr, maxCol, 0);
 	}
 	
-	private static int maxProfit(int[][] arr, int row, int col) {
+	private int maxProfit(int[][] arr, int row, int col) {
 		if(col == arr[0].length || row == arr.length || row < 0) {
 			return 0;
 		}
@@ -20,4 +23,45 @@ public class GoldMineProblem {
 		return arr[row][col] + Math.max(maxProfit(arr, row-1, col+1), maxProfit(arr, row + 1, col + 1));
 	}
 	
+	public List<Integer> maxProfitPath(int[][] arr) {
+		int maxRow = 0;
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i][0] > arr[maxRow][0]) {
+				maxRow = i;
+			}
+		}
+		
+		List<Integer> res = maxProfitPath(arr, maxRow, 0, null);
+		return res;
+	}
+
+	private List<Integer> maxProfitPath(int[][] arr, int row, int col, List<Integer> res) {
+		if(col == arr[0].length || row == arr.length || row < 0) {
+			return new ArrayList<Integer>();
+		}
+		
+		List<Integer> newList = new ArrayList<>();
+		List<Integer> upRight = maxProfitPath(arr, row-1, col+1, res); 
+		List<Integer> downRight = maxProfitPath(arr, row + 1, col + 1, res);
+		
+		if(sumOfElememts(upRight) > sumOfElememts(downRight)) {
+			newList.addAll(upRight);
+			newList.add(arr[row][col]);
+		} else {
+			newList.addAll(downRight);
+			newList.add(arr[row][col]);
+		}
+		
+		return newList;
+	}
+	
+	private int sumOfElememts(List<Integer> inp) {
+		int sum = 0;
+		
+		for(Integer i:inp) {
+			sum+=i;
+		}
+		
+		return sum;
+	}
 }
--- algonation/src/test/java/com/algonation/dp/GoldMineTest.java
+++ algonation/src/test/java/com/algonation/dp/GoldMineTest.java
@@ -5,13 +5,20 @@ import org.junit.Test;
 
 public class GoldMineTest {
 
+	GoldMineProblem gmp = new GoldMineProblem();
+	
 	int[][] arr = { {1, 3, 1, 5},
-            {2, 2, 4, 1},
-            {5, 0, 2, 3},
-            {0, 6, 1, 2}};
+		            {2, 2, 4, 1},
+		            {5, 0, 2, 3},
+		            {0, 6, 1, 2}};
 	
 	@Test
 	public void maxProfitTest() {
-		Assert.assertEquals(15, GoldMineProblem.maxProfit(arr));
+		Assert.assertEquals(15, gmp.maxProfit(arr));
+	}
+	
+	@Test
+	public void maxProfitPathTest() {
+		System.out.println(gmp.maxProfitPath(arr));
 	}
 }
