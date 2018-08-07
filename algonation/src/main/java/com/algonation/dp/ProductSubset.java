16aa56e1fea4bc7684eb29b59c705a3276a33d17
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2018-08-07 23:22:50 +0530

    Backup Commit

--- algonation/src/main/java/com/algonation/dp/ProductSubset.java
+++ algonation/src/main/java/com/algonation/dp/ProductSubset.java
@@ -4,21 +4,29 @@ public class ProductSubset {
 
 	public int countWithLesserProduct(int[] arr, int k) {
 		int currentProduct = 1;
-		return countWithLesserProduct(arr, currentProduct, k, arr.length-1);
+		return countWithLesserProduct(arr, currentProduct, k, 0);
 	}
-	
+
 	private int countWithLesserProduct(int[] arr, int currentProduct, int k, int n) {
-		if (n < 0 || currentProduct > k) {
+		if (currentProduct > k) {
 			return 0;
 		}
 		
-		return 1+countWithLesserProduct(arr, currentProduct * arr[n], k, n - 1)
-				+ countWithLesserProduct(arr, currentProduct, k, n - 1);
+		if(n == arr.length && currentProduct > k) {
+			return 0;
+		}
+		
+		if (n >= arr.length) {
+			return 1;
+		}
+
+		return countWithLesserProduct(arr, currentProduct * arr[n], k, n + 1)
+				+ countWithLesserProduct(arr, currentProduct, k, n + 1);
 	}
-	
+
 	public static void main(String[] args) {
 		ProductSubset ps = new ProductSubset();
-		int[] arr = {4, 8, 7, 2};
+		int[] arr = { 4, 8, 7, 2 };
 		int k = 50;
 		System.out.println(ps.countWithLesserProduct(arr, k));
 	}
